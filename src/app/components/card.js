27e0d5269f33c2f04c32a25cf329099e77ff8bfa
Evan Le coz <evanlecoz6@gmail.com>
2024-03-14 19:25:09 +0100

    Home added and responsive

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,14 +1,14 @@
 export default function Card({ title, description, image, textColor, inverse = false }) {
-    const containerClasses = `w-full h-fit flex ${inverse ? "flex-row-reverse" : "flex-row"}`;
+    const containerClasses = `w-full h-fit flex items-center 2xl:items-start ${inverse ? "flex-col 2xl:flex-row-reverse" : "flex-col 2xl:flex-row"}`;
 
   return (
     <div className={containerClasses}>
 
-      <img src={image} alt={title} className="w-fit" />
+      <img src={image} alt={title} className="w-fit rounded-[50px] 2xl:rounded-none px-3" />
 
-      <div className="px-10 w-full flex flex-col justify-center gap-5">
-      <h2 className="text-4xl font-bold text-sky-600" style={textColor}>{title}</h2>
-      <p className="text-black text-2xl">{description}</p>
+      <div className="px-10 w-full flex flex-col justify-center gap-5 text-center 2xl:text-left mt-5 2xl:mt-0">
+      <h2 className="text-2xl sm:text-4xl font-bold text-sky-600" style={textColor}>{title}</h2>
+      <p className="text-black text-base sm:text-2xl">{description}</p>
       </div>
     </div>
   );
--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="h-fit w-full rounded-tl-[50px] rounded-tr-[50px] bg-black mt-16 py-16 flex justify-center items-center gap-5 flex-col sm:flex-row">
             <Image src="/logoOlympiqueWhite.png" alt="Paris 2024" width={100} height={100} />
-            <Link href="/" className="text-white text-xl border-l-2 border-white pl-5">Accueil</Link>
+            <Link href="/" className="text-white text-xl pt-5 sm:pt-0 border-t-2 sm:border-t-none border-l-none sm:border-l-2 border-white pl-0 sm:pl-5">Accueil</Link>
             <Link href="/" className="text-white text-xl">Billetterie</Link>
             <Link href="/" className="text-white text-xl">Contact</Link>
             <span className="text-white text-xl">© Paris 2024</span>
