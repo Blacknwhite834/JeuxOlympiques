3f7c56837b5cd5cd3b04cf70a202ca0aa3fa29b9
Evan Le coz <evanlecoz6@gmail.com>
2024-03-20 19:20:40 +0100

    Page login and register fixed

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,6 +3,9 @@ import { redirect } from 'next/dist/server/api-utils';
 // login page front
 import { useEffect, useState } from 'react';
 import { useSession, signIn } from 'next-auth/react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Link from 'next/link';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -32,14 +35,22 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h2>Connexion</h2>
-      {message && <p>{message}</p>}
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <button type="submit">Se connecter</button>
+    <div className="flex flex-col min-h-screen">
+      <Header bgColor="bg-black" color={{ color: "black" }} borderColor="border-black"/>
+      <h1 className="text-4xl sm:text-6xl xl:text-8xl text-black font-bold text-center px-5">Veuillez vous connecter</h1>
+      <main className="flex-1 flex justify-center items-center">
+      {message && <p className=' text-red-500 font-bold text-base'>{message}</p>}
+      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center sm:items-start gap-5 w-full sm:w-fit px-5 sm:px-0 mt-5 sm:mt-10'>
+        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <div className='flex flex-col sm:flex-row justify-between items-center gap-3 w-full'>
+        <button type="submit" className='bg-black text-white px-10 py-2 rounded-md w-full hover:bg-opacity-70 transition duration-300'>Se connecter</button>
+        <Link href="/register" className='text-black p-2 rounded-md text-nowrap hover:underline'>Créer un compte ?</Link>
+        </div>
       </form>
+      </main>
+
+      <Footer />
     </div>
   );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,15 +1,25 @@
 "use client";
 // pages/register.js
 import { useState } from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Link from 'next/link';
 
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setMessage('Les mots de passe ne correspondent pas.');
+      return;
+    }
+
     const response = await fetch('/api/auth/register', {
       method: 'POST',
       headers: {
@@ -38,15 +48,24 @@ export default function Register() {
   }
 
   return (
-    <div>
-      <h2>Inscription</h2>
-      {message && <p>{message}</p>}
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nom" value={name} onChange={(e) => setName(e.target.value)} required/>
-        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <button type="submit">S'inscrire</button>
+    <div className="flex flex-col min-h-screen">
+      <Header bgColor="bg-black" color={{ color: "black" }} borderColor="border-black"/>
+      <h1 className="text-4xl sm:text-6xl xl:text-8xl text-black font-bold text-center px-5">Veuillez vous inscrire</h1>
+      <main className="flex-1 flex justify-center items-center">
+      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center sm:items-start gap-5 w-full sm:w-fit px-5 sm:px-0 mt-5 sm:mt-10'>
+        {message && <p className=' text-red-500 font-bold text-base'>{message}</p>}
+        <input type="text" placeholder="Nom" value={name} onChange={(e) => setName(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirmer mot de passe" value={confirmPassword} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <div className='flex flex-col sm:flex-row justify-between items-center gap-3 w-full'>
+        <button type="submit" className='bg-black text-white px-10 py-2 rounded-md w-full hover:bg-opacity-70 transition duration-300'>S'inscrire</button>
+        <Link href="/login" className='text-black p-2 rounded-md text-nowrap hover:underline'>Retourner en arrière</Link>
+        </div>
       </form>
+      </main>
+
+      <Footer />
     </div>
   );
 }
