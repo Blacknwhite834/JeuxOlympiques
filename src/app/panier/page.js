3eedb37a3705790d50e8ad15485895056500dd9c
Evan Le coz <evanlecoz6@gmail.com>
2024-03-22 14:08:55 +0100

    Cart fixed

--- src/app/panier/page.js
+++ src/app/panier/page.js
@@ -10,17 +10,22 @@ export default function Panier() {
     return (
         <div className="flex flex-col min-h-screen">
       <Header bgColor="bg-black" color={{ color: "black" }} borderColor="border-black"/>
-      <div className="flex-grow w-full flex justify-center">
+      <div className="flex-grow w-full flex flex-col">
         <h1 className="text-4xl sm:text-6xl xl:text-8xl text-black font-bold text-center px-5">Votre panier</h1>
-        <div className="">
+
+        <div className="mt-10 flex w-full justify-center gap-10">
+
+            <div className="">
+            <h2 className="text-2xl font-bold">Contenu de votre panier</h2>
             {cartItems.length > 0 ? (
                 <ul>
                     {cartItems.map((item) => (
                         <li key={item.id}>
-                            <div>
-                                <h2>{item.title}</h2>
-                                <p>{item.description}</p>
-                                <p>Prix: {item.prix}€</p>
+                            <div className="bg-zinc-100 rounded-lg p-5 my-5">
+                                <span className="text-xl font-bold">Offre:</span>
+                                <h2 className="text-xl ">{item.title}</h2>
+                                <p className="text-xl ">Nombre de billets: {item.nombre}</p>
+                                <p className="text-xl ">Prix: {item.prix}€</p>
                                 {/* Affichez ici d'autres détails que vous souhaitez montrer */}
                             </div>
                         </li>
@@ -29,9 +34,28 @@ export default function Panier() {
             ) : (
                 <p>Votre panier est vide.</p>
             )}
+            <button onClick={() => clearCart()} className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-opacity-70 transition duration-300 mt-5">Vider le panier</button>
+            </div>
+
+            <div className="bg-zinc-100 rounded-lg p-10 flex flex-col gap-3">
+            <h2 className="text-2xl font-bold">Récapitulatif de votre commande</h2>
+            <div className="h-0.5 bg-black my-5"></div>
+            <div className="flex justify-between">
+            <p>Nombre d'articles: </p><span className="font-bold">{cartItems.length}</span>
+            </div>
+            <div className="h-0.5 bg-zinc-300"></div>
+            <div className="flex justify-between">
+            <p>Nombre total de billets: </p><span className="font-bold">{cartItems.reduce((total, item) => total + item.nombre, 0)}</span>
+            </div>
+            <div className="h-0.5 bg-zinc-300"></div>
+            <div className="flex justify-between">
+            <p>Total: </p>
+            <span className="font-bold">{cartItems.reduce((total, item) => total + item.prix, 0)}€</span>
+            </div>
+            <button className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-opacity-70 transition duration-300 mt-5">Passer la commande</button>
+            </div>
 
 
-            <button onClick={() => clearCart()}>Vider le panier</button>
         </div>
         </div>
     </div>
