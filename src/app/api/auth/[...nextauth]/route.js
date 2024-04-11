5dbd635d15cd75db4718e27a17786d91cbc0d724
Evan Le coz <evanlecoz6@gmail.com>
2024-04-11 17:03:33 +0200

    Added next auth jwt

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -60,6 +60,9 @@ const handler = NextAuth({
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  }
   // Add other NextAuth configurations here
 });
 
