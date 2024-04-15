a7695ca3e85114cf5d337f8e8a12bddfe526c2c9
Evan Le coz <evanlecoz6@gmail.com>
2024-04-15 11:34:58 +0200

    middleware changed

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,20 @@
-import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
+// import { getToken } from "next-auth/jwt"
+// import { NextResponse } from "next/server"
 
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
 
-export const middleware = async (req) => {
-    // Add your own logic here or extend the middleware provided by NextAuth
-    const token = await getToken({ req, secret: process.env.SECRET })
-    const { pathname } = req.nextUrl
+// export const middleware = async (req) => {
+//     // Add your own logic here or extend the middleware provided by NextAuth
+//     const token = await getToken({ req, secret: process.env.SECRET })
+//     const { pathname } = req.nextUrl
 
-    // if (pathname.startsWith("/dashboard") && (!token || !(token.role === "ADMIN" || token.role === "ORGANISATEUR"))) {
-    //     // Redirect to login if the user is not authenticated
-    //     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`)
-    //   }
+//     // if (pathname.startsWith("/dashboard") && (!token || !(token.role === "ADMIN" || token.role === "ORGANISATEUR"))) {
+//     //     // Redirect to login if the user is not authenticated
+//     //     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`)
+//     //   }
     
 
-    return NextResponse.next()
-    }
+//     return NextResponse.next()
+//     }
 
-    export const config = { matcher: ["/dashboard/:path*"] }
+//     export const config = { matcher: ["/dashboard/:path*"] }
