80123dbcda84895da21f0cbf12c6d68a4487664f
Evan Le coz <evanlecoz6@gmail.com>
2024-04-17 15:19:11 +0200

    Added Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ import "./globals.css";
 export const metadata = {
   title: "Jeux Olympiques France 2024",
   description: "Les Jeux Olympiques de Paris 2024",
+  verification: {
+    google: 'J8M4l48WlK_IMGyT3MGNRB6mLEeH0SNI4q8MIAJ4J3M',
+  }
 };
 
 export default function RootLayout({ children }) {
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -49,7 +49,7 @@ export default function Login() {
       <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center sm:items-start gap-5 w-full sm:w-fit px-5 sm:px-0 mt-5 sm:mt-10'>
       {message && <p className=' text-red-500 font-bold text-base'>{message}</p>}
         <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
-        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl'/>
+        <input type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)} required className='py-2 w-full sm:w-fit px-5 pr-0 sm:pr-56 border-2 border-black rounded-full placeholder:text-black text-base sm:text-xl' />
         <div className='flex flex-col sm:flex-row justify-between items-center gap-3 w-full'>
         <button type="submit" className='bg-black text-white px-10 py-2 rounded-md w-full hover:bg-opacity-70 transition duration-300'>Se connecter</button>
         <Link href="/register" className='text-black p-2 rounded-md text-nowrap hover:underline'>Créer un compte ?</Link>
